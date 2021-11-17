4fa8a9a0786f8b4b688f5d5505ea55ca6f26464f
pizza-ll <736197113@qq.com>
2021-11-17 21:00:56 +0800

    test

--- webserver/main.go
+++ webserver/main.go
@@ -6,14 +6,15 @@ import (
 )
 
 func firstPage(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "<h1> Hello, this is my firstPage!<h1>")
+	io.WriteString(w, "<h1> Hello, I am Pizza!<h1>")
 }
 
 func main() {
 	http.HandleFunc("/", firstPage)
 	http.ListenAndServe(":8000", nil)
 }
+
 //手动完成的
 //1.git pull
-//2.git push-> git pull
+//2.git push -> git pull
 //3.deploy
